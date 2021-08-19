92073fe88b05ec5c8d995cc1bc0cc681e160b701
Nunu27 <wisnu.agung.001@gmail.com>
2021-08-19 16:31:22 +0700

    check the type of response

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ class ProxyScraper {
         method: "POST",
         mode: "cors",
       });
-      if (this.parseUrl) data = data.replace(/http.*?=.*?"/g, getURL);
+      if (typeof data === "string" && this.parseUrl)
+        data = data.replace(/http.*?=.*?"/g, getURL);
       return cheerio.load(data);
     } catch (error) {
       if (!alter) return await this.get(path, true);
