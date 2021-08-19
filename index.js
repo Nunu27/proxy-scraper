a71dcc935a8b4e8435fa561660ac0b79283b59ec
Nunu27 <wisnu.agung.001@gmail.com>
2021-08-19 15:42:12 +0700

    base url support

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-async function proxyScrape(path, alter = false) {
-  try {
-    const { data } = await axios({
-      url: alter
-        ? "https://i.nakedmaya.com/index.php"
-        : "http://duckproxy.com/indexa.php",
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-      },
-      data: `q=${encodeURIComponent(path)}&hl%5Bshow_images%5D=on`,
-      method: "POST",
-      mode: "cors",
-    });
-    return cheerio.load(data);
-  } catch (error) {
-    if (!alter) return await proxyRequest(path, true);
-    throw new Error(error);
+class ProxyScraper {
+  constructor(baseUrl = "") {
+    this.baseUrl = baseUrl;
+  }
+  async proxyScrape(path, alter = false) {
+    try {
+      const { data } = await axios({
+        url: alter
+          ? "https://i.nakedmaya.com/index.php"
+          : "http://duckproxy.com/indexa.php",
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+        },
+        data: `q=${encodeURIComponent(
+          this.baseUrl + path
+        )}&hl%5Bshow_images%5D=on`,
+        method: "POST",
+        mode: "cors",
+      });
+      return cheerio.load(data);
+    } catch (error) {
+      if (!alter) return await proxyRequest(path, true);
+      throw new Error(error);
+    }
   }
 }
 
-export default proxyScrape;
+export default ProxyScraper;
