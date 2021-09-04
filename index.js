cb9f7127edaa000f2c1d50314aa3bad7a3b44d58
Nunu27 <wisnu.agung.001@gmail.com>
2021-09-05 00:52:02 +0700

    added proxyUrl

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const cheerio = require("cheerio");
 const getURL = (url) => decodeURIComponent(url.split("=").pop());
 
 class ProxyScraper {
+  proxyUrl = (url) =>
+    `http://duckproxy.com/indexa.php?${new URLSearchParams({
+      q: url,
+      hl: 40,
+    })}`;
   constructor({ baseUrl = "", parseUrl = "all" }) {
     this.baseUrl = baseUrl;
     this.parseUrl = parseUrl;
